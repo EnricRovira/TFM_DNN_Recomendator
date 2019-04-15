5149913d07f235f8dd6e9d9c94aff98c81180108
Pareja2 <enric.rovira.melendez@alumnos.upm.es>
2019-04-15 21:00:48 +0200

    Version portatil

---
+++ 03_Preparacion_de_Datos/my_functions/__init__.py
@@ -0,0 +1,10 @@
+import re, string, unicodedata
+import nltk
+import contractions
+import inflect
+from bs4 import BeautifulSoup
+from nltk import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import LancasterStemmer, WordNetLemmatizer
+
+import preprocess
