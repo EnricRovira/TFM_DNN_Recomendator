ae22618b2e8847ff7b78f0587c087939efbd9758
Enric Rovira <enric.rovira96@gmail.com>
2019-06-03 23:43:08 +0200

    Add files via upload

--- 05_Implementacion/WebService_1.py
+++ 05_Implementacion/WebService_1.py
@@ -4,7 +4,7 @@
 #flask run --host=0.0.0.0
 #192.168.1.47:5000
 
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 from forms import form
 from tensorflow.keras.models import Model, Sequential, load_model
 import tensorflow as tf
@@ -21,25 +21,12 @@ def load_keras_model():
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    """Home page of app with form"""
-    # Create form
-    formulary = form.ReusableForm(request.form)
-
-    if request.method == 'POST' and formulary.validate():
-        # Extract information
-        customer = int(request.form['customer'])
-        top = int(request.form['top'])
-        print(customer, top)
-        load_keras_model()
-        return render_template('rec.html', input = r.recommend_1(model = rec_model, customer = customer, N = top))
-        #r.recommend_1(model = rec_model, customer = customer, N = top)
-
-    # Send template information to index.html
-    return render_template('index.html', form=formulary)
+    return '<a href="/rec"> click here for view your recommendations</a>'
 
 @app.route('/rec', methods=['GET', 'POST'])
 def rec():
-	return 
+    return None
+
 
 if __name__ == '__main__':
 	app.debug = True
